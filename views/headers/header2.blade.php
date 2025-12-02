3ed32aaa9c25d850bc333bdcf055d6f90282f57b
Edsell Muhindo <edsellmuhindo@gmail.com>
2025-12-03 00:27:15 +0300

    Initial commit: login page

--- views/headers/header2.blade.php
+++ views/headers/header2.blade.php
@@ -29,9 +29,9 @@
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ asset('bootstrap/assets/vendor/fonts/iconify-icons.css')}}" />
+    <link rel="stylesheet" href="{{ asset('bootstrap/assets/vending/fonts/iconify-icons.css')}}" />
 
-    <link rel="stylesheet" href="{{ asset('bootstrap/assets/vendor/css/core.css')}}" />
+    <link rel="stylesheet" href="{{ asset('bootstrap/assets/vending/css/core.css')}}" />
     <link rel="stylesheet" href="{{ asset('bootstrap/assets/css/demo.css')}}" />
 
 
